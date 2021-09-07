5bf43fffb3ddbe6f7044f607ea3c1bc19bc25a2c
danbugs <danilochiarlone@gmail.com>
2021-09-07 16:51:29 -0300

    some backend

--- clobber-server/index.js
+++ clobber-server/index.js
@@ -1,2 +1,10 @@
 // if you rename this file, don't forget to change the "server" field in Tarantella.toml
-require('./build/clobber-server');
+const Module = require('./build/clobber-server');
+
+Module['onRuntimeInitialized'] = function() {    
+    let clob = Module.allocateUTF8("This is a Clob from Node again!");
+    Module._make_clob(clob);
+    Module._free(clob);
+    let clobs = Module._get_clobs();
+    console.log(Module.UTF8ToString(clobs).trim().split('\n'));
+}
--- clobber-server/src/main.c
+++ clobber-server/src/main.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <emscripten.h>
+#include <time.h>
 
-EMSCRIPTEN_KEEPALIVE
-void print_hello() {
-    printf("Hello!\n");
+#include "../dependencies/dcs/dcs.h"
+
+extern Statement execute_sql(sql_t raw_sql, Table *table);
+extern Table *open_table(char *table_name);
+extern void close_table(Table *table);
+
+int generate_random_id()
+{
+    srand(time(NULL));
+    return rand();
 }
 
-int main() {
-    print_hello();
+EMSCRIPTEN_KEEPALIVE
+void make_clob(char *clob)
+{
+    Table *table = open_table("clob");
+    char new_insert[264]; // 264 = COLUMN_CONTENT_SIZE + 8 (for int size) + 1 (for '\0')
+    sprintf(new_insert, "INSERT %d %s", generate_random_id(), clob);
+    execute_sql(new_insert, table);
+    close_table(table);
+}
+
+EMSCRIPTEN_KEEPALIVE
+char* get_clobs()
+{
+    Table *table = open_table("clob");
+    sql_t new_select = SQL(SELECT);
+    Statement results = execute_sql(new_select, table);
+    close_table(table);
+    return results.select_result;
 }
